85c5c18f409bd673b5e8cf7e90ee83ba00bbe686
DannyRaafat <123320834+DannyRaafat@users.noreply.github.com>
2023-12-16 22:32:29 +0200

    Update RandomPlayer_game_2.cpp

--- src/RandomPlayer_game_2.cpp
+++ src/RandomPlayer_game_2.cpp
@@ -4,14 +4,21 @@
 #include<algorithm>
 #include"BoardGame_Classes.hpp"
 
-RandomPlayer_game_2::RandomPlayer_game_2 (char symbol, int dimension1 , int dimension2  )
-    : Player(symbol) , dimension1( dimension1 ) , dimension2( dimension2 ) {
-    this->name = "Random Computer Player";
+RandomPlayer_game_2::RandomPlayer_game_2 (char symbol, int dimension1,int dimension2, Board* brd ):
+        Player(symbol) , brd(brd) {
+    this->dimension1 = dimension1;
+    this->dimension2 = dimension2;
+    this->name = "Pyramid Computer Player";
     cout << "My names is " << name << endl;
 }
-
-// Generate a random move
 void RandomPlayer_game_2::get_move (int& x, int& y) {
-    x = rand() % dimension1 ;
-    y = rand() % dimension2 ;
+    int mv = -1 ;
+    for (int i = 0; i < 6 ; ++i) {
+        for (int j = 0; j < 7; ++j){
+            if ( brd->win_with( i , j ) >= mv ){
+                x = i , y = j ;
+                mv = brd->win_with( i , j ) ;
+            }
+        }
+    }
 }
